12874a0d6aa72ddc07652ef69a15cf51a85340e3
sbarberye <sbarberye@xebia.fr>
2021-12-03 16:35:41 +0100

    keycloak starter with adapter and application.properties

--- src/main/kotlin/com/xebia/keycloak/application/SkillsResourceApi.kt
+++ src/main/kotlin/com/xebia/keycloak/application/SkillsResourceApi.kt
@@ -1,30 +1,13 @@
 package com.xebia.keycloak.application
 
-import com.xebia.keycloak.application.configuration.OpenApiConfig
-import com.xebia.keycloak.application.error.ErrorHandlerController.ErrorDto
 import com.xebia.keycloak.application.model.SkillDto
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
-import io.swagger.v3.oas.annotations.security.SecurityRequirements
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @RequestMapping("/skills/v1/public")
-@SecurityRequirements(value = [SecurityRequirement(name = OpenApiConfig.BEARER_SCHEME)])
 interface SkillsResourceApi {
 
     @GetMapping(path = ["/view"])
-    @Operation(summary = "Find the authenticated user by jwt attribute userId")
-    @ApiResponses(
-        ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = SkillDto::class))]),
-        ApiResponse(responseCode = "404", description = "User not Found", content = [Content(schema = Schema(implementation = ErrorDto::class))]),
-        ApiResponse(responseCode = "403", description = "Forbidden"),
-        ApiResponse(responseCode = "401", description = "Unauthorized"),
-        ApiResponse(responseCode = "500", description = "Internal Server error")
-    )
+
     fun getSkills(): List<SkillDto>
 }
--- src/main/kotlin/com/xebia/keycloak/application/configuration/SecurityPublicApiConfig.kt
+++ src/main/kotlin/com/xebia/keycloak/application/configuration/SecurityPublicApiConfig.kt
@@ -1,50 +1,50 @@
 package com.xebia.keycloak.application.configuration
 
+import org.keycloak.adapters.KeycloakConfigResolver
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
-import org.springframework.core.convert.converter.Converter
-import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.config.web.servlet.invoke
-import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 
-@EnableWebSecurity
+
 @Configuration
-class SecurityPublicApiConfig : WebSecurityConfigurerAdapter() {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+class KeycloakSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
 
-
-
+    @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-    http
-    .csrf().disable()
-    .cors().disable()
-    .formLogin().disable()
-    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-    .and()
-    .authorizeRequests()
-    .antMatchers("/skills/v1/public/**").hasRole("payment")
-    .antMatchers(
-    "/actuator/**",
-    "/error",
-
-    "/skills/v1/api-docs/**",
-
-    ).permitAll()
-    .anyRequest().authenticated()
-    .and()
-    .oauth2ResourceServer().jwt().jwtAuthenticationConverter(getJwtAuthenticationConverter())
-
+        super.configure(http)
+        http.authorizeRequests()
+            .anyRequest()
+            .permitAll()
+        http.csrf().disable()
     }
-    private fun getJwtAuthenticationConverter(): Converter<Jwt, AbstractAuthenticationToken> {
-        return JwtAuthenticationConverter().apply {
-            setJwtGrantedAuthoritiesConverter(
-                KeyCloakGrantedAuthoritiesConverter()
-            )
-        }
+
+    @Autowired
+    @Throws(Exception::class)
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        val keycloakAuthenticationProvider = keycloakAuthenticationProvider()
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+        auth.authenticationProvider(keycloakAuthenticationProvider)
+    }
+
+    @Bean
+    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
+        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+    }
+
+    @Bean
+    fun KeycloakConfigResolver(): KeycloakConfigResolver {
+        return KeycloakSpringBootConfigResolver()
     }
 }
